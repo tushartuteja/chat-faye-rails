b2149b891570b19862862a38211dcfc0f545cd21
Tushar Tuteja <ttuteja@gmail.com>
2017-04-16 14:11:09 +0530

    added rest client

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,3 +1,4 @@
+require 'rest-client'
 class ChatController < ApplicationController
   before_action :authenticate_user!
   def index
@@ -13,8 +14,9 @@ class ChatController < ApplicationController
 			data["user"] = current_user.email
 			data["content"] = message
 			message = {:channel => "/messages/new", :data => data}
-			uri = URI.parse("http://localhost:9292/faye")
-			Net::HTTP.post_form(uri, :message => message.to_json)
+			response = RestClient.post("http://localhost:9292/faye", message.to_json, "content-type":"application/json")
+			# uri = URI.parse("http://localhost:9292/faye")
+			# Net::HTTP.post_form(uri, :message => message.to_json)
 		}
 	end  	
   end
