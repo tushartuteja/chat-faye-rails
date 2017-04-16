60ff3ea0418e0f0f4f35432566ce81482b5e3e5e
Tushar Tuteja <ttuteja@gmail.com>
2017-04-16 13:15:46 +0530

    initial commit

---
+++ app/controllers/chat_controller.rb
@@ -0,0 +1,22 @@
+class ChatController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  	@messages = Message.all
+  end
+
+  def new_message
+  	message = params[:message]
+  	@message = current_user.messages.create(:content => message)
+	respond_to do |format|
+		format.js{
+			data = Hash.new
+			data["user"] = current_user.email
+			data["content"] = message
+			message = {:channel => "/messages/new", :data => data}
+			uri = URI.parse("http://localhost:9292/faye")
+			Net::HTTP.post_form(uri, :message => message.to_json)
+		}
+	end  	
+  end
+
+end
